72b043d66f7e5e24696ba00535bb0de73d1a9aea
Thomas Bouvier <contact@thomas-bouvier.io>
2022-10-25 23:02:45 +0200

    feat: lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,4 +15,9 @@ packer.startup(function(use)
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
