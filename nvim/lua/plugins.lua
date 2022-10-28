93f5d322c55b04ed09652368e2284648a04afd3a
Thomas Bouvier <contact@thomas-bouvier.io>
2022-10-29 00:31:47 +0200

    feat(cmp): autocompletion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,26 +1,31 @@
 
 local status, packer = pcall(require, 'packer')
 if (not status) then
-	print("Packer is not installed")
-	return
+    print("Packer is not installed")
+    return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use 'neovim/nvim-lspconfig'
+    use 'onsails/lspkind-nvim'
+    use 'L3MON4D3/LuaSnip'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/nvim-cmp'
+    use 'neovim/nvim-lspconfig'
 
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'alexghergh/nvim-tmux-navigation'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use 'alexghergh/nvim-tmux-navigation'
 
-  use 'shaunsingh/nord.nvim'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'lewis6991/gitsigns.nvim'
+    use 'shaunsingh/nord.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'lewis6991/gitsigns.nvim'
 end)
