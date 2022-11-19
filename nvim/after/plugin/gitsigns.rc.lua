f29c52ec6ca4abb62272064444f75d2828af0063
Thomas Bouvier <contact@thomas-bouvier.io>
2022-11-19 22:08:45 +0100

    feat: `gitsigns` maps

--- nvim/after/plugin/gitsigns.rc.lua
+++ nvim/after/plugin/gitsigns.rc.lua
@@ -1,4 +1,24 @@
 local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+    on_attach = function(bufnr)
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Actions
+        map({'n', 'v'}, 'gs', ':Gitsigns stage_hunk<CR>')
+        map({'n', 'v'}, 'gr', ':Gitsigns reset_hunk<CR>')
+        map('n', 'gS', gitsigns.stage_buffer)
+        map('n', 'gu', gitsigns.undo_stage_hunk)
+        map('n', 'gR', gitsigns.reset_buffer)
+        map('n', 'gp', gitsigns.preview_hunk)
+        map('n', 'gb', function() gitsigns.blame_line{full=true} end)
+        map('n', 'gB', gitsigns.toggle_current_line_blame)
+        map('n', 'gd', gitsigns.diffthis)
+        map('n', 'gD', function() gitsigns.diffthis('~') end)
+    end
+}
