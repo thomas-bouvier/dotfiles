fb562a59a374e9eaec840b724af16f5b7fc49af0
Thomas Bouvier <contact@thomas-bouvier.io>
2024-08-29 17:40:05 +0200

    feat: add unstable url

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
@@ -14,31 +15,65 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       home-manager,
       plasma-manager,
+      sops-nix,
       ...
-    }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    }@inputs:
+    let
       system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./system/configuration.nix
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
+          inherit system;
+        };
+      };
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+        };
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-            users.thomas = import ./users/thomas;
-          };
-        }
-      ];
+        modules = [
+          (
+            { config, pkgs, ... }:
+            {
+              nixpkgs.overlays = [ overlay-unstable ];
+            }
+          )
+
+          ./system/configuration.nix
+
+          # https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nixos-module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "bak";
+
+              sharedModules = [
+                plasma-manager.homeManagerModules.plasma-manager
+                inputs.sops-nix.homeManagerModules.sops
+              ];
+              users.thomas = import ./users/thomas;
+            };
+          }
+        ];
+      };
     };
-  };
 }
