a84c47a815f29b3d575f9832b7e9e7cabcd79d91
Thomas Bouvier <contact@thomas-bouvier.io>
2025-01-09 18:30:57 +0100

    refactor(ssh): use proper attributes

--- users/thomas/ssh.nix
+++ users/thomas/ssh.nix
@@ -4,28 +4,34 @@
     enable = true;
 
     matchBlocks = {
-      "hosts" = {
+      "github" = {
         host = "github.com";
         identitiesOnly = true;
         identityFile = [
           "~/.ssh/id_ed25519"
         ];
       };
-    };
 
-    extraConfig = ''
-      Host g5k
-        User tbouvier
-        Hostname access.grid5000.fr
-        ForwardAgent no
-      Host *.g5k
-        User tbouvier
-        ProxyCommand ssh g5k -W "$(basename %h .g5k):%p"
-        ForwardAgent no
-      Host !access.grid5000.fr *.grid5000.fr
-        ProxyCommand ssh -A tbouvier@194.254.60.33 -W "$(basename %h):%p"
-        User tbouvier
-        ForwardAgent yes
-    '';
+      "g5k" = {
+        host = "g5k";
+        user = "tbouvier";
+        hostname = "access.grid5000.fr";
+        forwardAgent = false;
+      };
+  
+      "*.g5k" = {
+        host = "*.g5k";
+        user = "tbouvier";
+        proxyCommand = "ssh g5k -W \"$(basename %h .g5k):%p\"";
+        forwardAgent = false;
+      };
+
+      "access.g5k" = {
+        host = "!access.grid5000.fr *.grid5000.fr";
+        proxyCommand = "ssh -A tbouvier@194.254.60.33 -W \"$(basename %h):%p\"";
+        user = "tbouvier";
+        forwardAgent = true;
+      };
+    };
   };
 }
