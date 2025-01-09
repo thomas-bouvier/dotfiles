3f3395a4a25ec716e0b7d1f695e3d540db8ac4b9
Thomas Bouvier <contact@thomas-bouvier.io>
2025-01-09 21:48:23 +0100

    feat: add hardware configuration for an old machine
    This commit includes the hardware configuration details for an old
    Intel NUC from 2015 that is still in use by relatives. The machine
    had Windows 10 installed on it since July 2015.

---
+++ hosts/README.md
@@ -0,0 +1,6 @@
+# hosts
+
+| Hostname | Type | Architecture | CPU | RAM | Graphics |
+|----------|----------|----------|-------|------|---|
+| `bolet` | A PC in a Cooler Master case, which I've been upgrading since 2012 | x86_64 | 12 × AMD Ryzen 5 5500 | 16 Go | NVIDIA GeForce GTX 1050 Ti |
+| `coprin` | Intel NUC (2015) | x86_64 | 2 × Intel Celeron N3050 | 4 Go | Mesa Intel HD Graphics 4000 |
---
+++ hosts/coprin/default.nix
@@ -0,0 +1,63 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+      ../../system/configuration.nix
+
+      # Users
+      ../../users/thomas.nix
+      ../../users/famille.nix
+    ];
+
+  boot = {
+    # Use GRUB as this machine relies on BIOS.
+    loader.grub.enable = true;
+    loader.grub.device = "/dev/sda";
+
+    initrd.availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_acpi" ];
+    initrd.kernelModules = [ ];
+
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
+
+  networking.hostName = "coprin";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # https://discourse.nixos.org/t/nixos-rebuild-remote-deployments-non-root-pam/50477
+  security.pam = {
+    sshAgentAuth.enable = true;
+    services.sudo.sshAgentAuth = true;
+  };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/ROOT";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [
+    {
+      device = "/.swapfile";
+    }
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp1s0f0u10.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
