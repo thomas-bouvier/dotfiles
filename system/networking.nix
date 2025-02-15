5bf1d1ece44d02925d9461960616433c9712a016
Thomas Bouvier <contact@thomas-bouvier.io>
2025-02-15 13:56:35 +0100

    feat: open mDNS port
    Useful for printer discovery.

--- system/networking.nix
+++ system/networking.nix
@@ -8,8 +8,9 @@
 
       # Localsend: port 53317
       # CUPS: port 631
+      # mDNS: port 5353
       allowedTCPPorts = [ 53317 631 ];
-      allowedUDPPorts = [ 53317 631 ];
+      allowedUDPPorts = [ 53317 631 5353 ];
     };
 
     # Use dns0.eu
