178a96a03ab5e0d7dd0a8dc316d20b4606b7a039
Thomas Bouvier <contact@thomas-bouvier.io>
2025-11-22 09:55:04 +0100

    feat(amanite): add huge swap device

--- hosts/amanite/default.nix
+++ hosts/amanite/default.nix
@@ -42,7 +42,14 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/swapfile";
+    size = 60 * 1024; # 60GB
+  }];
+  zramSwap = {
+    enable = true;
+    memoryPercent = 90;
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
