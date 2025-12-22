57bff6ae0a5a2d98c853e729dbc92bd719fef47f
Thomas Bouvier <contact@thomas-bouvier.io>
2025-12-22 12:18:55 +0100

    feat(thomas): update wallpaper

--- users/thomas/default.nix
+++ users/thomas/default.nix
@@ -34,8 +34,8 @@ in
   # Stylix configuration for user-specific wallpaper
   stylix = {
     image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/thomas-bouvier/wallpapers/main/davis-county.jpg";
-      sha256 = "sha256-fd4K7qyQM0k3ffowrBBmhbyPYeeUoU8hMr5rZ33hUAI=";
+      url = "https://raw.githubusercontent.com/thomas-bouvier/wallpapers/main/ecmwf.png";
+      sha256 = "sha256-m9DzW+B5As/FemMObk2Kll+Nf2B3uYaHD8EXJl0w+pU=";
     };
   };
 
