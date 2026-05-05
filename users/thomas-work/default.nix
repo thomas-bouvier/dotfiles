20acc370c2f27339d2ec5f3c8c94c75146834494
Thomas Bouvier <contact@thomas-bouvier.io>
2026-05-05 10:34:43 +0200

    fix(thomas-work): typo

--- users/thomas-work/default.nix
+++ users/thomas-work/default.nix
@@ -123,7 +123,7 @@ in
     # You should not change this value, even if you update Home Manager. If you do
     # want to update the value, then make sure to first check the Home Manager
     # release notes.
-    home.stateVersion = "24.05"; # Please read the comment before changing.
+    stateVersion = "24.05"; # Please read the comment before changing.
   };
 
   gtk.gtk4.theme = null;
