cd6d237a89179a0e9e79dcafc8218f86e3621f58
Thomas Bouvier <contact@thomas-bouvier.io>
2026-05-05 18:41:01 +0200

    fix(ssh): use explicit config

--- users/thomas/ssh.nix
+++ users/thomas/ssh.nix
@@ -2,8 +2,22 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+
       "github" = {
         host = "github.com";
         identitiesOnly = true;
